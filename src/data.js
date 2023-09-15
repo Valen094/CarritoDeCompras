14dfc447fdd94d2435ddb1f882e72ee3b65ee7ae
valen094 <ramirezvalentina783@gmail.com>
2023-09-15 14:47:43 -0500

    finalizado

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://i.pinimg.com/originals/7a/e8/bb/7ae8bbf40204592c535fbadb3311c95b.png',
+		nameProduct: 'Cajita - Gomitas enchiladas',
+		price: 6000,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://i.pinimg.com/originals/ad/be/db/adbedb2a515ba4f03a0a1d17eebf0002.png',
+		nameProduct: 'Caja grande con gomitas',
+		price: 32000,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://i.pinimg.com/originals/74/ff/56/74ff56495011a53d44737d47f269eaef.png',
+		nameProduct: 'Mango enchilado',
+		price: 4000,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'https://i.pinimg.com/originals/c7/15/e6/c715e67c222247eb3b85fe4db3568b89.png',
+		nameProduct: 'Gomitas enchiladas',
+		price: 2500,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://i.pinimg.com/originals/fd/15/2c/fd152c42a2d652f5c9e6b16e9cb21623.png',
+		nameProduct: 'Gomitas',
+		price: 2000,
+		quantity: 1,
+	},
+	{
+		id: 6,
+		img: 'https://i.pinimg.com/originals/d8/f5/06/d8f5067476d2fcfd5e857e43c9557b0e.png',
+		nameProduct: 'Audifonos',
+		price: 2500,
+		quantity: 1,
+	},
+	{
+		id: 7,
+		img: 'https://i.pinimg.com/originals/05/45/85/054585fa9f05c10c88ac1064ca085135.png',
+		nameProduct: 'Mango con gomitas',
+		price: 5000,
+		quantity: 1,
+	},
+	{
+		id: 8,
+		img: 'https://i.pinimg.com/originals/ee/dd/29/eedd295c688819d6a6079f3a60e8af40.png',
+		nameProduct: 'Enchilados',
+		price: 5000,
+		quantity: 1,
+	},
+	{
+		id: 9,
+		img: 'https://i.pinimg.com/originals/8e/41/8e/8e418e618414ea6468629714a1656406.png',
+		nameProduct: 'Cajita enchilada',
+		price: 6000,
+		quantity: 1,
+	}
+	// Aquí pueden agregar más o modificar los productos existentes
+];
